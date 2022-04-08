8ed6b0e4d6fdf44fbbd0e8f644dcd2909cc17026
Laura Bécognée <jaimaille@gmail.com>
2022-04-08 18:40:44 +0200

    forgotten a single quote

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def findone():
             database_arguments["mongo-collection"],
             database_arguments["mongo-filter"]
         )
-        return "{\"result':\""+str(result).replace("\'", "\"")+"\"}"
+        return "{\"result\":\""+str(result).replace("\'", "\"")+"\"}"
     else:
         return "{\"result\":\"database not recognized\"}"
 
